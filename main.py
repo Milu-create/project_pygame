1ba13c976e7d2dc93e382df852c1798b9aca51a7
vadimkocetok <koshetok.com@mail.ru>
2021-01-20 00:32:25 +0300

    Добавлена страница с рекордами

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ class App:
 
             dt = self._clock.tick()
             self._state.loop(dt)
+            font = pygame.font.Font(None, 30)
             pygame.display.flip()
         self._state.destroy()
 
@@ -63,9 +64,10 @@ class AppState:
 
 
 class MenuState(AppState):
-
     def __init__(self, background_image):
         super().__init__()
+        self._but_new_game = Button()
+        self._record_but = Button()
         self._bg_img = imgs[background_image]
 
     def setup(self):
@@ -73,41 +75,41 @@ class MenuState(AppState):
         self._bg_img = pygame.transform.scale(self._bg_img, (WIDTH, HEIGHT))
 
     def process_event(self, event):
-        if event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN):
-            self.get_app().set_state(GameState())
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self._but_new_game.pressed(pygame.mouse.get_pos()):
+                self.get_app().set_state(GameState())
+            elif self._record_but.pressed(pygame.mouse.get_pos()):
+                self.get_app().set_state(RecState('recbackground'))
 
     def loop(self, dt):
         screen = self.get_app().get_screen()
         screen.fill((0, 0, 0))
         screen.blit(self._bg_img, (0, 0))
-        self._worki = App()
-        self._but_new_game = Button(self._worki.get_screen(), (66, 170, 255), 500, 350, 200, 50, 0, "GO", (35, 52, 110))
+        self._but_new_game.do(screen, (66, 170, 255), 500, 350, 200, 50, 0, "PLAY", (35, 52, 110))
+        self._record_but.do(screen, (66, 170, 255), 500, 650, 200, 50, 0, "RECORDS", (35, 52, 110))
         font = pygame.font.Font(None, 30)
 
     def destroy(self):
         pass
 
 
-class Button:
-    def __init__(self, surface, color, x, y, length, height, width, text, text_color):
-        self.draw_button(surface, color, length, height, x, y, width)
-        self.write_text(surface, text, text_color, length, height, x, y)
-        self._rect = pygame.Rect(x, y, length, height)
+class RecState(AppState):
+    def __init__(self, background_image):
+        super().__init__()
+        self._bg_img = imgs[background_image]
 
-    def write_text(self, surface, text, text_color, length, height, x, y):
-        font_size = int((length - 100) // len(text))
-        myFont = pygame.font.SysFont("Calibri", font_size)
-        myText = myFont.render(text, True, text_color)
-        surface.blit(myText, ((x + length / 2) - myText.get_width() / 2, (y + height / 2) - myText.get_height() / 2))
+    def setup(self):
+        pass
 
-    def draw_button(self, surface, color, length, height, x, y, width):
-        for i in range(1, 10):
-            s = pygame.Surface((length + (i * 2), height + (i * 2)))
-            s.fill(color)
-            surface.blit(s, (x - i, y - i))
-        pygame.draw.rect(surface, color, (x, y, length, height), width)
+    def process_event(self, event):
+        pass
 
-    def pressed(self, mouse):
+    def loop(self, dt):
+        screen = self.get_app().get_screen()
+        screen.fill((0, 0, 0))
+        screen.blit(self._bg_img, (0, 0))
+
+    def destroy(self):
         pass
 
 
@@ -130,6 +132,41 @@ class GameState(AppState):
         pass
 
 
+class Button:
+    def do(self, surface, color, x, y, length, height, width, text, text_color):
+        self.draw_button(surface, color, length, height, x, y, width)
+        self.write_text(surface, text, text_color, length, height, x, y)
+        self._rect = pygame.Rect(x, y, length, height)
+
+    def write_text(self, surface, text, text_color, length, height, x, y):
+        font_size = int((length - 100) // len(text))
+        myFont = pygame.font.SysFont("Calibri", font_size)
+        myText = myFont.render(text, True, text_color)
+        surface.blit(myText, ((x + length / 2) - myText.get_width() / 2, (y + height / 2) - myText.get_height() / 2))
+
+    def draw_button(self, surface, color, length, height, x, y, width):
+        for i in range(1, 10):
+            s = pygame.Surface((length + (i * 2), height + (i * 2)))
+            s.fill(color)
+            surface.blit(s, (x - i, y - i))
+        pygame.draw.rect(surface, color, (x, y, length, height), width)
+
+    def pressed(self, mouse):
+        if mouse[0] > self._rect.topleft[0]:
+            if mouse[1] > self._rect.topleft[1]:
+                if mouse[0] < self._rect.bottomright[0]:
+                    if mouse[1] < self._rect.bottomright[1]:
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+
 def load_image(image_path, colorkey=None):
     result = pygame.image.load(image_path)
     if colorkey is not None:
@@ -145,7 +182,7 @@ if __name__ == '__main__':
     app = App()
 
     imgs = {'background': load_image('background.png'),
-            }
+            'recbackground': load_image('recback.png')}
 
     menu_state = MenuState('background')
     app.set_state(menu_state)
